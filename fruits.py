d0b2c0bbec615a0ec17beadbf1c95df151a15638
MIKEZZZ-KING999 <micigwubor@gmail.com>
2024-12-05 17:11:36 +0000

    Add files via upload

---
+++ fruits.py
@@ -0,0 +1,10 @@
+sample_list = [apples, bananas, cherry, orange]
+
+sample_set = set(sample_list)
+print(sample_set)
+
+print(sample_set[tangerine])
+if 3 in sample_set:
+    print("Yes")
+else:
+    print("No")   
